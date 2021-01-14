d07c91d372f15f65681d87775d14869c072a46a7
Caleb Matteis <matteismusic@gmail.com>
2021-01-14 07:13:45 -0500

    created bigrams.js file

---
+++ bigram.js
@@ -0,0 +1,8 @@
+// Textual markov chain generator using bigrams.
+
+class MarkovMachine {
+    // build markov machime; read in text
+    constructor(text) {
+        let words = text.split(/[\r\n]+/);
+    }
+}
